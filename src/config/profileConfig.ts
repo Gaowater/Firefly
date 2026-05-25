fb279dd374f1c14bfc54c3f5cfbbbfea1650d5cc
Gaowater <3384357097@qq.com>
2026-05-25 20:20:15 +0800

    Update profileConfig.ts

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -9,10 +9,10 @@ export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/avatar.avif",
 
 	// 名字
-	name: "Firefly",
+	name: "牧河伊琉",
 
 	// 个人签名
-	bio: "Hello, I'm Firefly.",
+	bio: "每日小确幸，从爱好自己开始！",
 
 	// 链接配置
 	// 已经预装的图标集：fa7-brands，fa7-regular，fa7-solid，material-symbols，simple-icons
@@ -24,7 +24,7 @@ export const profileConfig: ProfileConfig = {
 		{
 			name: "qq",
 			icon: "fa7-brands:qq",
-			url: "https://qm.qq.com/q/ZGsFa8qX2G",
+			url: "tencent://AddContact/?fromId=45&fromSubId=1&subcmd=all&uin=3384357097",
 			showName: false,
 		},
 		{
